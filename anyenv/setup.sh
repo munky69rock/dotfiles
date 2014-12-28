e95bbb0eaa22016b4ef3a57641563b1360eca8c5
Masayuki Uehara <munky69rock@gmail.com>
2014-12-28 20:54:02 +0900

    update some rcfiles

--- anyenv/setup.sh
+++ anyenv/setup.sh
@@ -4,3 +4,6 @@ ANYENV_ROOT=${ANYENV_ROOT:-~/.anyenv}
 ANYENV_URL=https://github.com/riywo/anyenv
 
 git clone $ANYENV_URL $ANYENV_ROOT
+
+mkdir -p $ANYENV_ROOT/plugins
+git clone https://github.com/znz/anyenv-update.git $ANYENV_ROOT/anyenv-update
--- ruby/setup-rbenv.sh
+++ ruby/setup-rbenv.sh
@@ -3,6 +3,8 @@
 RUBY_VERSION=${RUBY_VERSION:-2.0.0-p0}
 RBENV_URL=https://github.com/sstephenson/rbenv.git
 RUBY_BUILD_URL=https://github.com/sstephenson/ruby-build.git
+RBENV_DEFAULT_GEMS_URL=https://github.com/sstephenson/rbenv-default-gems.git
+RBENV_UPDATE_URL=https://github.com/rkh/rbenv-update.git
 
 RBENV_ROOT=${RBENV_ROOT:-~/lib/ruby/rbenv}
 PLUGIN_DIR=$PYENV_ROOT/plugins
@@ -13,6 +15,8 @@ git clone $RBENV_URL $RBENV_ROOT
 # install ruby-build
 mkdir -p $PLUGIN_DIR
 git clone $RUBY_BUILD_URL $PLUGIN_DIR/ruby-build
+git clone $RBENV_DEFAULT_GEMS_URL $PLUGIN_DIR/rbenv-default-gems
+git clone $RBENV_UPDATE_URL $PLUGIN_DIR/rbenv-update
 
 # load rbenv
 PATH=$RBENV_ROOT/bin:$PATH
