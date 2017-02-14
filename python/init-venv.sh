80dca23057e62282d1dbb26c6073046881482977
Masayuki Uehara <uehara.masayuki@dena.jp>
2017-02-14 21:56:44 +0900

    Removed unused files

--- python/init-venv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python -m venv --without-pip .
-source bin/activate
-curl https://bootstrap.pypa.io/get-pip.py | python -
-if which direnv 2&> /dev/null; then
-  direnv edit .
-fi
--- python/install-pip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl https://bootstrap.pypa.io/get-pip.py | python -
