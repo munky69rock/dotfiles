48601d6cd01864d72fa59fa2ddf883d101adf758
Masayuki Uehara <munky69rock@gmail.com>
2020-09-11 17:59:49 +0900

    feat(vim): use nvim if exists

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,6 +7,12 @@ elif which dtruss &> /dev/null; then
   alias strace=dtruss
 fi
 
+if which nvim &> /dev/null; then
+  alias vi=nvim
+  alias vim=nvim
+  alias vimdiff=nvim -d
+fi
+
 [ -f $HOME/.aliases.local ] && source $HOME/.aliases.local
 
 # vim: syn=sh :
